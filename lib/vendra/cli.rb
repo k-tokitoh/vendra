990b79a1b92a946c70f72f58ae05cf1792846ffa
takashi.suzuki <42316228+k-tokitoh@users.noreply.github.com>
2021-02-26 15:02:22 +0900

    create config file

--- lib/vendra/cli.rb
+++ lib/vendra/cli.rb
@@ -4,9 +4,12 @@ require "thor"
 
 module Vendra
   class CLI < Thor
+    include Thor::Actions
+
     desc "this is summary.", "this is detailed description."
     def example
       p "command called."
+      create_file "~/.vendra/example.conf", "your vendra config"
     end
   end
 end
